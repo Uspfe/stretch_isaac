a7e9cd5bf84c879855cd85c3b51e3244697d9513
Uspfe <benjamin.bogenberger@tum.de>
2026-01-21 14:31:31 +0100

    cleanup sim script

--- standalone_sim.py
+++ standalone_sim.py
@@ -1,19 +1,13 @@
 import argparse
 import csv
-import json
-import time
 from pathlib import Path
-from typing import Literal, Optional, Union
 
 import numpy as np
 from isaacsim import SimulationApp
-from matplotlib import pyplot as plt
-
-from utils.multi_dijkstra import MultiDijkstra
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Run standalone simulation with optional scene selection.")
+    parser = argparse.ArgumentParser(description="Run standalone simulation with optional scene selection. Given 'goal_asset', the shortest path to the goal asset is computed and its distance printed periodically. Additional assets can be spawned and existing assets removed based on substrings.")
     parser.add_argument(
         "--headless",
         action="store_true",
@@ -75,19 +69,6 @@ def parse_args() -> argparse.Namespace:
         default=[],
     )
     parser.add_argument(
-        "--generate-map",
-        type=Path,
-        help="Path to save the generated occupancy map in a .npz file.",
-        default=None,
-    )
-    parser.add_argument(
-        "--generate-map-objs",
-        type=str,
-        help="Objects whose positions to store additionally in the .npz map file (by substring match of the prim path).",
-        nargs="*",
-        default=[],
-    )
-    parser.add_argument(
         "--disable-scene-collider",
         action="store_true",
         help="Disable the scene collider.",
@@ -99,9 +80,6 @@ def parse_args() -> argparse.Namespace:
     )
 
     args = parser.parse_args()
-
-    if args.generate_map is not None and args.generate_map.suffix != ".npz":
-        raise ValueError("generate-map path must end with .npz")
     
     if args.scene is not None and not args.scene.exists():
         raise FileNotFoundError(f"Scene file {args.scene} does not exist.")
@@ -113,14 +91,23 @@ args = parse_args()
 app = SimulationApp({"headless": args.headless})
 
 # issacsim imports only become availbele after `SimulationApp` is created
-import carb  # pyright: ignore[reportMissingImports] # noqa: E402
 import omni  # pyright: ignore[reportMissingImports] # noqa: E402
 import omni.kit.actions.core  # pyright: ignore[reportMissingImports] # noqa: E402
 from isaacsim.core.api import World  # pyright: ignore[reportMissingImports] # noqa: E402
 from isaacsim.core.utils import extensions  # pyright: ignore[reportMissingImports] # noqa: E402
 from omni.isaac.core.articulations import Articulation  # pyright: ignore[reportMissingImports] # noqa: E402
 from omni.isaac.core.utils.stage import add_reference_to_stage  # pyright: ignore[reportMissingImports] # noqa: E402
-from pxr import Gf, PhysxSchema, Sdf, Usd, UsdGeom  # pyright: ignore[reportMissingImports] # noqa: E402
+
+from utils.isaac_utils import (  # noqa: E402
+    disable_collision,
+    dump_prim_position,
+    dump_state,
+    get_shortest_path_to_prims,
+    get_toplevel_prims_substring,
+    hide_prim,
+    set_prim_pose,
+    switch_lighting,
+)
 
 
 def read_colors(csv_path: Path) -> dict:
@@ -131,280 +118,11 @@ def read_colors(csv_path: Path) -> dict:
             colors[row["object"]] = [float(row["r"]), float(row["g"]), float(row["b"])]
     return colors
 
-
-def compute_occupancy_map(
-    root_prim_path: str,
-    resolution: float,
-    width_m: float,
-    height_m: float,
-    z_min: float,
-    z_max: float,
-    return_color: bool = False,
-    prim_colors: dict = {},
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """
-    Computes a 2D occupancy map under the given root prim.
-    Uses fixed grid size (width, height) and cell resolution.
-    Returns a numpy bool array.
-    """
-    stage = omni.usd.get_context().get_stage()
-    root = stage.GetPrimAtPath(root_prim_path)
-
-    if not root.IsValid():
-        raise ValueError(f"Invalid prim path: {root_prim_path}")
-
-    # Root AABB (only to determine map center)
-    bbox = UsdGeom.Boundable(root).ComputeWorldBound(0, "default").GetRange()
-    # bbox_size = bbox.GetMax() - bbox.GetMin()  # Gf.Vec3f
-    width = int(np.ceil(width_m / resolution))
-    height = int(np.ceil(height_m / resolution))
-
-    center = (bbox.GetMin() + bbox.GetMax()) * 0.5
-
-    xs = center[0] + (np.arange(width) * resolution - width * resolution * 0.5)
-    ys = center[1] + (np.arange(height) * resolution - height * resolution * 0.5)
-
-    occ = np.zeros((width, height))
-    color = np.zeros((width, height, 3))  # RGB map
-
-    physx = omni.physx.get_physx_scene_query_interface()
-
-    cell_offsets = [
-        (0, 0),  # center
-        (-0.5, -0.5),
-        (-0.5, 0.5),
-        (0.5, -0.5),
-        (0.5, 0.5),
-    ]
-
-    def on_hit(hit):
-        return True
-
-    for ix, x in enumerate(xs):
-        for iy, y in enumerate(ys):
-            num_hits = physx.overlap_box(
-                carb.Float3(resolution * 0.5, resolution * 0.5, (z_max - z_min) / 2),
-                carb.Float3(x, y, (z_min + z_max) / 2),
-                carb.Float4(1.0, 0.0, 0.0, 0.0),
-                on_hit,
-                True,
-            )
-            if num_hits > 0:
-                occ[ix, iy] = 1
-
-            if not return_color:
-                continue
-
-            if num_hits == 0:
-                color[ix, iy] = (1, 1, 1)  # white
-                continue
-
-            # cast multiple rays to get top prim and base the color on that
-            prim_hit_counts = {}
-            for dx_frac, dy_frac in cell_offsets:
-                rx = x + dx_frac * resolution
-                ry = y + dy_frac * resolution
-                start = carb.Float3(rx, ry, z_max)
-                direction = carb.Float3(0, 0, -1)
-                hit = physx.raycast_closest(start, direction, z_max - z_min, False)
-                if hit["hit"]:
-                    prim_path = hit["collision"]
-                    prim_hit_counts[prim_path] = prim_hit_counts.get(prim_path, 0) + 1
-
-            if prim_hit_counts:
-                # choose prim with most hits
-                top_prim = max(prim_hit_counts, key=lambda k: prim_hit_counts[k])
-
-                for key in prim_colors:
-                    if key in top_prim:  # substring match
-                        color[ix, iy] = prim_colors[key]
-                        break
-                else:
-                    # fallback: assign random color
-                    prim_colors[top_prim] = np.random.rand(3)
-                    color[ix, iy] = prim_colors[top_prim]
-            else:
-                # fallback black
-                color[ix, iy] = (0, 0, 0)
-
-    return occ, xs, ys, color
-
-
-def get_shortest_path_to_prims(
-    prims: list[Usd.Prim],
-    start_position: np.ndarray = np.zeros(2),
-    resolution: float = 0.1,
-    map_width: float = 20.0,
-    map_height: float = 20.0,
-    z_min: float = 0.2,
-    z_max: float = 1.8,
-    visualize: bool = False,
-) -> Optional[tuple[float, np.ndarray]]:
-    if len(prims) == 0:
-        return None
-    goal_positions = dump_prim_position(prims, print_output=False)
-    occupancy_map, x, y, _ = compute_occupancy_map(
-        root_prim_path="/Root", resolution=resolution, width_m=map_width, height_m=map_height, z_min=z_min, z_max=z_max
-    )
-    multi_dijkstra = MultiDijkstra(
-        occupancy_map.T, resolution=resolution, origin=np.array([x[0], y[0]]), approx_downsample_resolution=None
-    )
-    _costs, dists, paths = multi_dijkstra.get_min_distance_to_goals(start_position, goal_positions[:, :2])
-    i = np.argmin(dists)
-    dist = dists[i]
-    path = paths[i]
-    print(f"Computed shorted path with distance {dist}")
-
-    if visualize:
-        X, Y = np.meshgrid(x, y, indexing="ij")
-        plt.pcolormesh(X, Y, 1 - occupancy_map, cmap="gray", shading="auto")
-        plt.scatter(start_position[0], start_position[1], c="green", marker="o", label="Start")
-        plt.scatter(goal_positions[:, 0], goal_positions[:, 1], c="red", marker="x", label="Goals")
-        plt.plot(path[:, 0], path[:, 1], c="blue", linewidth=2, label="Path")
-        plt.gca().set_aspect("equal")
-        plt.title("Occupancy Map")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.show()
-
-    return dist, goal_positions, path
-
-
-def switch_lighting(mode: Literal["camera", "stage"] = "camera"):
-    # switch lighting
-    action_registry = omni.kit.actions.core.get_action_registry()
-    action = action_registry.get_action("omni.kit.viewport.menubar.lighting", "set_lighting_mode_" + mode)
-    action.execute()
-
-
-def get_visibility_attribute(stage: Usd.Stage, prim_path: str) -> Union[Usd.Attribute, None]:
-    """Return the visibility attribute of a prim"""
-    path = Sdf.Path(prim_path)
-    prim = stage.GetPrimAtPath(path)
-    if not prim.IsValid():
-        return None
-    visibility_attribute = prim.GetAttribute("visibility")
-    return visibility_attribute
-
-
-def hide_prim(stage: Usd.Stage, prim_path: str):
-    """Hide a prim
-
-    Args:
-        stage (Usd.Stage, required): The USD Stage
-        prim_path (str, required): The prim path of the prim to hide
-    """
-    visibility_attribute = get_visibility_attribute(stage, prim_path)
-    if visibility_attribute is None:
-        return
-    visibility_attribute.Set("invisible")
-
-
-def show_prim(stage: Usd.Stage, prim_path: str):
-    """Show a prim
-
-    Args:
-        stage (Usd.Stage, required): The USD Stage
-        prim_path (str, required): The prim path of the prim to show
-    """
-    visibility_attribute = get_visibility_attribute(stage, prim_path)
-    if visibility_attribute is None:
-        return
-    visibility_attribute.Set("inherited")
-
-
-def dump_state(
-    time: float,
-    position: tuple[float, 3],
-    orientation: tuple[float, 4],
-    linear_velocity: tuple[float, 3],
-):
-    data = {
-        "time": time,
-        "position": {"x": position[0], "y": position[1], "z": position[2]},
-        "orientation": {
-            "w": orientation[0],
-            "x": orientation[1],
-            "y": orientation[2],
-            "z": orientation[3],
-        },
-        "linear_velocity": {
-            "vx": linear_velocity[0],
-            "vy": linear_velocity[1],
-            "vz": linear_velocity[2],
-        },
-    }
-    print("<robot>" + json.dumps(data) + "</robot>")
-
-
-def dump_prim_position(
-    prims: list[Usd.Prim], shortest_distance: Optional[float] = None, print_output: bool = True
-) -> np.ndarray:
-    positions = np.ndarray((len(prims), 3))
-    data = {}
-    for i, prim in enumerate(prims):
-        xformable = UsdGeom.Xformable(prim)
-        world_matrix = xformable.ComputeLocalToWorldTransform(Usd.TimeCode.Default())
-        pos = world_matrix.ExtractTranslation()
-        data[prim.GetName()] = {"x": round(pos[0], 2), "y": round(pos[1], 2), "z": round(pos[2], 2)}
-        positions[i, :] = [pos[0], pos[1], pos[2]]
-    if shortest_distance is not None:
-        data["shortest_distance"] = round(shortest_distance, 2)
-    if print_output:
-        print("<goals>" + json.dumps(data) + "</goals>")
-    return positions
-
-
 def parse_assets(raw_assets):
     assets = []
     for name, x, y, z, theta in raw_assets or []:
         assets.append((name, float(x), float(y), float(z), float(theta)))
     return assets
-
-
-def get_toplevel_prims_substring(
-    search_root: Usd.Prim, prim_substring: list[str], references_only: bool = False
-) -> list[Usd.Prim]:
-    matched_prims = []
-    for prim in Usd.PrimRange(search_root):
-        prim_name = prim.GetName()
-
-        has_payload = prim.HasPayload()
-        has_reference = prim.HasAuthoredReferences()
-        valid = (not references_only) or (has_reference or has_payload)
-        if has_payload or has_reference:
-            print(f"{prim_name}: payload={has_payload}, reference={has_reference}")
-
-        if any(
-            (valid and substring in prim_name and substring not in str(prim.GetPath().GetParentPath()))
-            for substring in prim_substring
-        ):
-            matched_prims.append(prim)
-    return matched_prims
-
-
-def set_prim_pose(prim, pos, theta):
-    xform = UsdGeom.Xformable(prim)
-
-    # translate
-    ops = xform.GetOrderedXformOps()
-    t_op = next((op for op in ops if op.GetOpName() == "xformOp:translate"), None)
-    if t_op is None:
-        t_op = xform.AddTranslateOp()
-    t_op.Set(Gf.Vec3d(*pos))
-
-    # rotate Z
-    r_op = xform.AddRotateZOp()
-    r_op.Set(float(theta))
-
-
-def disable_collision(root_prim: Usd.Prim):
-    for prim in Usd.PrimRange(root_prim):
-        collision_api = PhysxSchema.PhysxCollisionAPI.Apply(prim)
-        attr = collision_api.GetPrim().GetAttribute("physics:collisionEnabled")
-        if attr:
-            attr.Set(False)
-
 
 def main(simulation_app, args: argparse.Namespace):
     args.asset = parse_assets(args.asset)
@@ -419,9 +137,14 @@ def main(simulation_app, args: argparse.Namespace):
     shortest_goal_distance = None
     if args.scene is not None:
         print(f"Loading scene from {args.scene}")
+
+        # load scene
         omni.usd.get_context().open_stage(str(args.scene))
+        loaded_scene_root = "/Root"
         world = World()
-        _scene = world.stage.GetPrimAtPath("/Root")
+        _scene = world.stage.GetPrimAtPath(loaded_scene_root)
+
+        # hide specified assets
         hide_assets = get_toplevel_prims_substring(_scene, args.rasset, True)
         for prim in hide_assets:
             if any(exclude in prim.GetName() for exclude in args.rasset_exclude):
@@ -430,6 +153,7 @@ def main(simulation_app, args: argparse.Namespace):
             print(f"Hiding prim {prim.GetPath()}")
             hide_prim(world.stage, str(prim.GetPath()))
 
+        # find goal assets
         print(f"Searching for goal assets with substring: {args.gasset}")
         goal_assets = get_toplevel_prims_substring(_scene, [args.gasset]) if args.gasset is not None else []
         goal_assets = [
@@ -438,80 +162,40 @@ def main(simulation_app, args: argparse.Namespace):
 
         world.reset()
 
-        # disable collision of hidden assests before computing the map
+        # disable collision of hidden assets before computing the shortest distances to the goal assets
         for prim in hide_assets:
             disable_collision(prim)
 
+        # compute shortest path to goal assets
         print("Computing shortest path to goals...")
         if len(goal_assets) > 0:
-            shortest_goal_distance, goal_positions, shortest_path = get_shortest_path_to_prims(goal_assets)
+            shortest_goal_distance, goal_positions, shortest_path = get_shortest_path_to_prims(goal_assets, start_position=np.array(args.robot_start[0:2]), root_prim_path=loaded_scene_root)
             if shortest_goal_distance is not None:
                 shortest_goal_distance -= 1.5  # viewing distance offset
                 print(f"Shortest distance to goal assets (with offset): {round(shortest_goal_distance, 2)}")
-        else:
-            goal_positions = np.zeros((0, 3))
-            shortest_path = np.zeros((0, 2))
 
-        if args.generate_map is not None:
-            prim_colors = read_colors(Path("./interior_agent_objects.csv"))
-            _occupancy_map, x, y, colored_map = compute_occupancy_map(
-                root_prim_path="/Root",
-                resolution=0.1,
-                width_m=20,
-                height_m=20,
-                z_min=0.2,
-                z_max=1.8,
-                return_color=True,
-                prim_colors=prim_colors,
-            )
-
-            if len(args.generate_map_objs) > 0:
-                additional_assets = get_toplevel_prims_substring(_scene, args.generate_map_objs)
-                print(
-                    f"Found additional assets for map generation: {[prim.GetPath() for prim in additional_assets]} for substrings {args.generate_map_objs}"
-                )
-                additional_positions = dump_prim_position(additional_assets, print_output=False)
-            else:
-                additional_positions = np.ndarray((0, 3))
-
-            np.savez_compressed(
-                args.generate_map,
-                colored_map=colored_map,
-                x=x,
-                y=y,
-                goal_positions=goal_positions,
-                shortest_path=shortest_path,
-                additional_positions=additional_positions,
-            )
-            # print state once, so parent process know isaac sim started successfully
-            dump_state(
-                0.0,
-                (0.0, 0.0, 0.0),
-                (1.0, 0.0, 0.0, 0.0),
-                (0.0, 0.0, 0.0),
-            )
-            time.sleep(1.0)  # ensure file is written
-            print(f"Saved map to {args.generate_map}")
-            exit(0)
-
+        # disable scene collider if requested
         if args.disable_scene_collider:
             print(f"Disabling collision for scene {_scene.GetPath()}")
             disable_collision(_scene)
     else:
         world = World()
         world.reset()
-
+    
+    # add ground plane
     ground_plane = world.scene.add_default_ground_plane(prim_path=root_prim + "/defaultGroundPlane", z_position=0.05)
     if args.scene is not None:
         hide_prim(world.stage, ground_plane.prim_path)
 
+    # set lighting mode
     print(f"Setting lighting mode to {args.lighting}")
     switch_lighting(mode=args.lighting)
 
     # load robot
-    stretch_asset_path = Path("./robot_usd/stretch3_no_arm_collider.usd").absolute().as_posix()
+    stretch_asset_path = Path("./robot_usd/stretch3.usd").absolute().as_posix()
     prim_stretch = add_reference_to_stage(usd_path=stretch_asset_path, prim_path=root_prim)
 
+    # load additional assets
     for id, asset in enumerate(args.asset):
         asset_usd_path, x, y, z, theta = asset
         name = Path(asset_usd_path).stem
@@ -522,6 +206,7 @@ def main(simulation_app, args: argparse.Namespace):
         set_prim_pose(prim_asset, (x, y, z), theta)
     world.reset()
 
+    # initialize robot articulation
     stretch = Articulation(prim_path=str(prim_stretch.GetPath()) + "/stretch")
     stretch.set_world_pose(
         np.array([args.robot_start[0], args.robot_start[1], args.robot_start[2]]),
@@ -529,6 +214,7 @@ def main(simulation_app, args: argparse.Namespace):
     )
     stretch.initialize()
 
+    # main simulation loop
     print_pose_interval: int = 33
     print_goal_interval: int = 110
     try:
@@ -536,6 +222,8 @@ def main(simulation_app, args: argparse.Namespace):
         while simulation_app.is_running():
             world.step(render=True)  # execute one physics step and one rendering step
             step_count += 1
+
+            # periodically print robot pose
             if step_count % print_pose_interval == 0:
                 position: np.ndarray
                 orientation: np.ndarray
@@ -547,8 +235,12 @@ def main(simulation_app, args: argparse.Namespace):
                     orientation.tolist(),
                     linear_velocity.tolist(),
                 )
+
+            # periodically print goal asset positions
             if step_count % print_goal_interval == 0:
                 dump_prim_position(goal_assets, shortest_goal_distance)
+
+            # reset counter periodically to avoid overflow
             if step_count > 1e4:
                 step_count = 0
     except KeyboardInterrupt:
